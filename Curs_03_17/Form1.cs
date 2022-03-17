0afe5735ca11bbdab28d1283545ec91ee7d08f2f
Alexandru Ifrim <ifrim.alexandru0@gmail.com>
2022-03-17 10:08:18 +0200

    Add project files.

---
+++ Curs_03_17/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Curs_03_17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TextReader note = new StreamReader(@"../../TextFile1.txt");
+
+            string t1 = note.ReadLine();
+            string t2 = note.ReadLine();
+            
+            int[] v;
+
+            int s = 0;
+            int n = int.Parse(t1);
+            v = new int[n];
+            string[] t3 = t2.Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                v[i] = int.Parse(t3[i]);
+                s = s + v[i];
+            }  
+           
+            listBox1.Items.Add(t1);
+            listBox1.Items.Add(t2);
+            listBox1.Items.Add(s.ToString());
+
+        }
+    
+    }
+}
